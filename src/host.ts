08d06d6d2b599c9d3e7dab6e1d0969fe748513f8
BabakMahmoudi <babak@gnco.ir>
2022-01-17 13:15:32 +0330

    host

--- src/host.ts
+++ src/host.ts
@@ -5,13 +5,13 @@ import tomcat from ".";
 tomcat.Infrastructure.Base.Logger.level = 'debug'
 tomcat.Infrastructure.Base.Logger.getLogger("WebSocketHub").level = 'info'
 const port = 8084;
-const ip = "hub"
+const ip = tomcat.utils.ipAddress();
 //const url = `http://${ip}:${port}/hub`;
 tomcat.config.setServer(ip, port);
 //tomcat.config.infrastructure.messaging.transports.websocket.url = url;
-const hub = tomcat.getHostBuilder('hub')
-    .addWebSocketHub()
-    .buildWebHost('express')
+// const hub = tomcat.getHostBuilder('hub')
+//     .addWebSocketHub()
+//     .buildWebHost('express')
 
 
 const server = tomcat.getHostBuilder('server')
